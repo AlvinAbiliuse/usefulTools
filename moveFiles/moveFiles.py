d7f1794e5e6f7d0b661d6ab2f382ad729052affb
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-20 03:27:20 +0400

    moveFiles.py

--- moveFiles/moveFiles.py
+++ moveFiles/moveFiles.py
@@ -7,14 +7,14 @@ def move(location):
     paths = os.listdir(f"./{location}")
     p = paths[random.randint(0, len(paths) - 1)]
     print(f"moving {p}")
-    shutil.copy(f"./{location}/{p}", "./")
+    shutil.move(f"./{location}/{p}", "./")
 
 
 def copy(argv):
     print(argv[1])
     print(len(os.listdir(f"./{argv[2]}")) - 1)
     if int(argv[1]) >= len(os.listdir(f"./{argv[2]}")) - 1:
-        for i in range(len(os.listdir(f"./{argv[2]}")) - 1):
+        for i in range(len(os.listdir(f"./{argv[2]}"))):
             move(argv[2])
     else:
         for i in range(int(argv[1])):
