c492b006d3f1d94bb50787f37891544a022e71b5
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-20 03:07:08 +0400

    moveFiles.py

--- moveFiles/moveFiles.py
+++ moveFiles/moveFiles.py
@@ -3,12 +3,20 @@ import sys
 import shutil
 import random
 
+def move(argv):
+    paths = os.listdir(f"./{argv[2]}");
+    p = paths[random.randint(0, len(paths))]
+    shutil.copy(f"./{argv[2]}/{p}", "./")
+
+
 def copy(argv):
     print(argv)
-    for i in range(int(argv[1])):
-        paths = os.listdir(f"./{argv[2]}");
-        p = paths[random.randint(0, len(paths))]
-        shutil.copy(f"./{argv[2]}/{p}", "./")
+    if int(argv[1]) < len(os.listdir(f"./{argv[2]}")):
+        for i in range(len(os.lisrdir(f"./{argv[2]}"))):
+            move(argv[2])
+    elif:
+        for i in range(int(argv[1])):
+            move(argv[2])
 
 copy(sys.argv)
 
