5e3d6dd0c1ac588e7c65efc858c791f88a257577
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-20 03:02:21 +0400

    moveFiles.py

--- moveFiles/moveFiles.py
+++ moveFiles/moveFiles.py
@@ -7,8 +7,8 @@ def copy(argv):
     print(argv)
     for i in range(int(argv[1])):
         paths = os.listdir(f"./{argv[2]}");
-        print(random.randint(0, len(paths)))
-
+        p = paths[random.randint(0, len(paths))]
+        shutil.copy(f"./{argv[2]}/{p}", "./")
 
 copy(sys.argv)
 
