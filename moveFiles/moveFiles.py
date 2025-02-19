0bd6cd40cb9df8e2d81979de231f54a5f0ca668d
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-20 02:53:54 +0400

    moveFiles.py

--- moveFiles/moveFiles.py
+++ moveFiles/moveFiles.py
@@ -1,9 +1,13 @@
 import os
 import sys
 import shutil
+import random
 
 def copy(argv):
     print(argv)
+    for i in range(argv[1]):
+        paths = os.listdir(f"./{argv[2]}");
+        print(random.randint(0, len(paths)))
 
 
 copy(sys.argv)
