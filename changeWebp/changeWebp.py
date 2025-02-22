e4886453c17f99d1c8ce7d22d86a514ca2c2efa0
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-22 14:37:03 +0400

    changeWebp.py

--- changeWebp/changeWebp.py
+++ changeWebp/changeWebp.py
@@ -6,9 +6,10 @@ import sys
 arg = sys.argv[1]
 
 for i in os.listdir(arg):
-    os.makedirs(f"./{arg} copy", exist_ok=True)
+    os.makedirs(f"./copy/{arg}", exist_ok=True)
+    print(i)
     if (i.split(".")[-1] == "webp"):
             im = Image.open(f"./{arg}/{i}").convert("RGB")
-            im.save(f"./{arg} copy/{i.replace('webp', '')}jpg")
+            im.save(f"./copy/{arg}/{i.replace('webp', '')}jpg")
     else:
-        shutil.copy(f"./{arg}/{i}", f"./{arg} copy");
+        shutil.copy(f"./{arg}/{i}", f"./copy/{arg}");
